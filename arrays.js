8bb88c539fe889dfffec2d27b9463993018055e0
= <=>
2025-08-25 19:06:58 +0200

    Opgave 1 løst

--- arrays.js
+++ arrays.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
-   
+     //opgave 1: 
+     //  et nyt array, som indeholder alle de tal som er større end 20.
+    let res1 = numbersArray.filter((elm) => elm > 20)
+    console.log(res1);
 }) // ends DOMContentLoaded
 
